158b5fc8c4ddc55b05a16eaf1eace9b1bfb8a800
IRedDragonICY <hendik.suwoto@gmail.com>
2025-04-07 00:11:51 +0700

    Revert version and versionCode in module.prop to previous state

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,2 +1,3 @@
-#!/system/bin/sh
-until service list|grep -q ^SurfaceFlinger;do sleep 1;done;service call SurfaceFlinger 1008 i32 1;exit 0
+MODDIR=${0%/*}
+
+until service list | grep -q ^SurfaceFlinger; do service call SurfaceFlinger 1008 i32 1;done
