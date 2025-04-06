7b8e40221851860f012312138695e81256229e6f
IRedDragonICY <hendik.suwoto@gmail.com>
2025-04-06 23:47:20 +0700

    Fix service script to include sleep interval for SurfaceFlinger check

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,4 +1,2 @@
 #!/system/bin/sh
-MODDIR=${0%/*}
-
-until service list | grep -q ^SurfaceFlinger; do service call SurfaceFlinger 1008 i32 1;done;exit 0
+until service list|grep -q ^SurfaceFlinger;do sleep 1;done;service call SurfaceFlinger 1008 i32 1;exit 0
