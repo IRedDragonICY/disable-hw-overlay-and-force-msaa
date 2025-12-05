2a025a5113db4567ea7f7113728033a6da8bec96
Ndik <98265563+IRedDragonICY@users.noreply.github.com>
2025-12-05 11:00:36 +0700

    Update service check for SurfaceFlinger

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,3 +1,3 @@
 MODDIR=${0%/*}
 
-until service list | grep -q ^SurfaceFlinger; do service call SurfaceFlinger 1008 i32 1;done
+until service list | grep -q SurfaceFlinger; do sleep 1; done; service call SurfaceFlinger 1008 i32 1
